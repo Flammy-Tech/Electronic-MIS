4b3595fe00f7469eb2933b2c3a97d02adfd0a0cc
Lewis Ng'ang'a <59686000+lewis-2000@users.noreply.github.com>
2023-10-24 12:39:17 +0300

    Initialize Project
    Initialized Project With the Home Router and Page

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app= express();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+//Static files
+app.use(express.static('public'))
+app.use('css', express.static(__dirname + 'public/css'));
+app.use('js', express.static(__dirname + 'public/js'));
+app.use('assets', express.static(__dirname + 'public/assets'));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
+// JSON request body parser
+app.use(express.json());
+// URL-encoded request body parser
+app.use(express.urlencoded({ extended: true })); 
+
+
+//routes
+app.use('/', require('./routes/home'));
+// app.use('/account', require('./routes/account'));
+// app.use('/materials', require('./routes/materials'));
+// app.use('/search', require('./routes/search'));
+// app.use('/addBook', require('./routes/addBook'));
+
+
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+});
